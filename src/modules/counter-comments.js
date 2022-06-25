d0e180082d2bfbb38693a709d8f6100a170f0b06
Menanya <emmmiemenz@gmail.com>
2022-06-25 02:45:08 +0100

    first commit

---
+++ src/modules/counter-comments.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-expressions */
+
+const commentsCounter = (data) => {
+  let counter = data.length;
+  if (!counter) counter = 0;
+  return counter;
+};
+
+export default commentsCounter;
